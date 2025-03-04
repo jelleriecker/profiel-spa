0b9a76d0cd915e7e16e52548699c0d5392d14673
Jelle Riecker <jelle@riecker.org>
2025-03-04 17:59:32 +0100

    editted email

--- profiel.riecker.org/profile/src/components/contact/Contact.js
+++ profiel.riecker.org/profile/src/components/contact/Contact.js
@@ -6,8 +6,7 @@ const Contact = ({ id }) => {
   return (
     <section id={id} className="Contact">
       <h1>Contact Me</h1>
-      <p>Email: recruitment-contact.directed192@passfwd.com</p>
-      <p1>yes, the e-mail is correct i'm just using aliasses =) </p1>
+      <p>Email: contact@riecker.org</p>
       <p>LinkedIn: <a href="https://www.linkedin.com/in/jelle-riecker">Jelle Riecker</a></p>
       <p>Github: <a href="https://github.com/jelleriecker">Jelle Riecker</a></p>
 
