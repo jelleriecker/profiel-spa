464f098ce2f3c0cc6319e96375d5c8e40272a967
Jelle Riecker <jelle@riecker.org>
2025-03-11 17:00:24 +0100

    smaal update

--- jelle.riecker.org/profile/src/App.js
+++ jelle.riecker.org/profile/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { } from 'react';
 import Sidebar from './components/sidebar/Sidebar';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
